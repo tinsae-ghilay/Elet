d64c5f28816d1891867936ec18236dc316a93fa7
tinsae-ghilay <85639930+tinsae-ghilay@users.noreply.github.com>
2023-04-18 19:54:28 +0100

    fixed app locale change inconsistency

--- app/src/main/java/com/tgk/elet/MainActivity.kt
+++ app/src/main/java/com/tgk/elet/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.tgk.elet
 
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tgk.Elet.R
 import com.tgk.Elet.databinding.ActivityMainBinding
 import com.tgk.elet.common.CommonViewModel
+import com.tgk.elet.common.Preferences.getPreferences
 import com.tgk.elet.common.Preferences.setAppLocale
 import com.tgk.elet.common.Util
 import com.tgk.elet.common.Util.format
@@ -18,16 +23,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     private val cViewModel: CommonViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         // first and for most , we get locale here
-        setAppLocale(this)
+        setAppLocale(pref)
         Util.setTemporalArrays(this)
-
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val toolBar = binding.toolbar
@@ -42,10 +44,27 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
     }
+    private val pref:SharedPreferences by lazy { this.getPreferences() }
 
     override fun onResume() {
         super.onResume()
-        setAppLocale(this)
+        // have to register this
+        pref.registerOnSharedPreferenceChangeListener (prefListener)
     }
+
+    override fun onPause() {
+        // have to unregister this
+        super.onPause()
+        pref.unregisterOnSharedPreferenceChangeListener(prefListener)
+    }
+
+    private val prefListener:OnSharedPreferenceChangeListener =
+        OnSharedPreferenceChangeListener { p, key ->
+            if (key == "lang") {
+                val appLocale = LocaleListCompat
+                    .forLanguageTags(p.getString(key, "ti"))
+                AppCompatDelegate.setApplicationLocales(appLocale)
+            }
+        }
 
 }
--- app/src/main/java/com/tgk/elet/common/Preferences.kt
+++ app/src/main/java/com/tgk/elet/common/Preferences.kt
@@ -9,23 +9,10 @@ import androidx.preference.PreferenceManager
 object Preferences {
 
     fun Context.getPreferences():SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-    fun setAppLocale(context: Context) {
-        val pref = context.getPreferences()
-        pref.registerOnSharedPreferenceChangeListener { p, key ->
-            if (key == "lang") {
-                val appLocale = LocaleListCompat
-                    .forLanguageTags(p.getString(key, "ti"))
-                AppCompatDelegate.setApplicationLocales(appLocale)
-                /*val dialog = AlertDialog.Builder(this).create()
-                dialog.setMessage(getString(R.string.reboot_message))
-                dialog.setButton(AlertDialog.BUTTON_POSITIVE,getString(R.string.yes)) { d, _ ->
 
-                        d.dismiss()
-                        this.recreate()
-                }
-                dialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.no)){d, _ -> d.dismiss()}
-                dialog.show()*/
-            }
-        }
+    fun setAppLocale(preferences: SharedPreferences){
+        val appLocale = LocaleListCompat
+            .forLanguageTags(preferences.getString("lang", "ti"))
+        AppCompatDelegate.setApplicationLocales(appLocale)
     }
 }
