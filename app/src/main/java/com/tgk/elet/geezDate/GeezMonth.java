516849a1dd2b4dd22a45897b47c09220fa83622e
tinsae-ghilay <85639930+tinsae-ghilay@users.noreply.github.com>
2024-02-12 00:53:54 +0100

    Update GeezMonth.java

--- app/src/main/java/com/tgk/elet/geezDate/GeezMonth.java
+++ app/src/main/java/com/tgk/elet/geezDate/GeezMonth.java
@@ -15,16 +15,20 @@ public class GeezMonth extends Month {
         return MonthName.values()[ getMonth()-1 ];
     }
 
+    // calculates number of days a month has
     protected int getSize(){ return  (!isPagumien())? 30 : (getYear() % 4 == 3)? 6 : 5;}
 
+    // determine if Month is Pagumen
     protected boolean isPagumien(){ return getMonth() == 13; }
 
+    // validate month
     @Override
     protected void validate() {
         super.validate();
         if (this.getMonth() > 13) throw new IllegalArgumentException("Geez month should not exceed 13");
     }
-
+   
+    // generats days of this month object
     private void populateDates() {
         GeezDate date = GeezDate.of(getYear(),getMonth(),1);
         date = date.plusDays(-getBahtiIndex());
